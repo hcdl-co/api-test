afddb77cbd0452f241edc4ef9a76e77471c730b3
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2022-11-20 19:16:20 -0800

    data is displaying on page

--- src/App.js
+++ src/App.js
@@ -1,90 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import API from "./utils/BlueCross_API";
+import React, { useEffect, useState } from "react";
 
-function ShowResponse(res) {
-  let providerList = [res]
-  console.log(providerList[0][0])
- 
-if (res) {
+const App = () => {
+    const [advice, setAdvice] = useState("");
 
-  console.log(res.name);
-  console.log(res.length);
-  console.log(res.type);
-  // return (
-  //   <p>{res}</p>
-  // )
-}
+    useEffect(() => {
+      let nameQuery = "john";
+      let url = `https://public.fhir.flex.optum.com/R4/HealthcareService?service-category=prov&name=${nameQuery}`;
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                console.log(json.entry);
+
+                for(let i = 0; i < json.entry.length)
 
 
+                setAdvice(JSON.stringify(json.entry));
 
-// }
-}
+            } catch (error) {
+                console.log("error", error);
+            }
+        };
 
+        fetchData();
+    }, []);
 
-export default function App() {
-
-
-  const searchProviders = async () => {
-   await API.getProviders().then (res => {
-    // ShowResponse(res);
-    console.log(res)
-    console.log(res.length)
-   }
-    )
-  
-  }
-
-  useEffect(() =>
-    searchProviders
-  )
-
- 
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="flex-grid">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h3 >Healthcare Download</h3>
+    return (
+        <div>
+            <p>{advice}</p>
         </div>
-        <h5>Api Tester</h5>
-      </header>
+    );
+};
 
-
-      <main>
-      <button onClick={searchProviders}>Test</button>
-        {/* <div className="flex-grid">
-          <div className="col">This little piggy went to market.</div>
-          <div className="col">This little piggy stayed home.</div>
-          <div className="col">This little piggy had roast beef.</div>
-        </div> */}
-     
-    
-          <ShowResponse/>
-
-        
-
-        
-      
-        <div className="flex-grid">
-          <div className="col">This little piggy went to market.</div>
-          <div className="col">This little piggy stayed home.</div>
-          <div className="col">This little piggy had roast beef.</div>
-          <div className="col">This little piggy had none.</div>
-          <div className="col">This little piggy went wee wee wee all the way home.</div>
-        </div>
-
-        {/* <div className="flex-grid-thirds">
-          <div className="col">This little piggy went to market.</div>
-          <div className="col">This little piggy stayed home.</div>
-          <div className="col">This little piggy had roast beef.</div>
-        </div> */}
-      </main>
-
-    </div>
-  );
-}
-
-
+export default App;
--- src/utils/BlueCross_API.js
+++ src/utils/BlueCross_API.js
@@ -1,11 +1,11 @@
-// import React, { useState, useEffect } from 'react'
-// export default function () {
+import React, { useState, useEffect } from 'react'
+
 
 
 const API = {
     
     getProviders: async function() {
-    let nameQuery = "david";
+    let nameQuery = "john";
     let apiLink = `https://public.fhir.flex.optum.com/R4/HealthcareService?service-category=prov&name=${nameQuery}`;
     let providerArr = [];
     fetch(apiLink)
@@ -13,13 +13,13 @@ const API = {
             // console.info(res.json());
             return res.json();
         })
-        .then((pdata) => {
-            console.log("Data: ", pdata);
-            console.log("Data Entry: ", pdata.entry);
+        .then((allProvider) => {
+            // console.log("Data: ", allProvider);
+            // console.log("Data Entry: ", allProvider.entry);
         
 
-            for (let i = 0; i < pdata.entry.length; i++) {
-                let locationQuery = pdata.entry[i].resource.location[0].reference;
+            for (let i = 0; i < allProvider.entry.length; i++) {
+                let locationQuery = allProvider.entry[i].resource.location[0].reference;
                 let locationLink = `https://public.fhir.flex.optum.com/R4/${locationQuery}`;
                 fetch(locationLink)
                     .then((res) => {
@@ -28,81 +28,33 @@ const API = {
                     })
                     .then((ldata) => {
                         let provider = {
-                            name: pdata.entry[i].resource.name,
-                            telephone: pdata.entry[i].resource.telecom,
-                            role: pdata.entry[i].resource.category[0].coding[0].display,
+                            name: allProvider.entry[i].resource.name,
+                            telephone: allProvider.entry[i].resource.telecom,
+                            role: allProvider.entry[i].resource.category[0].coding[0].display,
                             address: ldata.address.text
                         }
-                        localStorage.setItem(JSON.stringify(provider.name), [JSON.stringify(provider.name)])
-                        if (pdata.entry[i].resource.specialty) {
-                            let specialityArr = [];
-                            for (let j = 0; j < pdata.entry[i].resource.specialty.length; j++) {
-                                specialityArr.push(pdata.entry[i].resource.specialty[j].text);
-                            }
-                            provider.speciality = specialityArr;
-                        }
+                        localStorage.setItem(
+                            JSON.stringify(provider.name),
+                             [
+                                JSON.stringify(provider.name),
+                                JSON.stringify(allProvider.entry[i].resource.telecom[0].value),
+                                JSON.stringify(allProvider.entry[i].resource.category[0].coding[0].display),
+                                JSON.stringify(ldata.address.text),
+                                JSON.stringify(allProvider.entry[i].resource.specialty)
+                            ])
+                   
                         providerArr.push(provider);
-                         console.log(providerArr[0].name);
-                        // return providerArr;
-                        // localStorage.setItem(JSON.stringify(providerArr[i].name));
+               
                     })
                    
             }
             
-          
+           
         })
         return providerArr;
     }
    
 }
 
-// async function bcAPI() {
-
-//     // api link here
-//     let nameQuery = "david";
-//     let apiLink = `https://public.fhir.flex.optum.com/R4/HealthcareService?service-category=prov&name=${nameQuery}`;
-
-//     fetch(apiLink)
-//         .then((res) => {
-//             console.info(res);
-//             return res.json();
-//         })
-//         .then((pdata) => {
-//             console.log("Data: ", pdata);
-//             console.log("Data Entry: ", pdata.entry);
-//             let providerArr = [];
-
-//             for (let i = 0; i < pdata.entry.length; i++) {
-//                 let locationQuery = pdata.entry[i].resource.location[0].reference;
-//                 let locationLink = `https://public.fhir.flex.optum.com/R4/${locationQuery}`;
-//                 fetch(locationLink)
-//                     .then((res) => {
-//                         //console.info(res);
-//                         return res.json();
-//                     })
-//                     .then((ldata) => {
-//                         let provider = {
-//                             name: pdata.entry[i].resource.name,
-//                             telephone: pdata.entry[i].resource.telecom,
-//                             role: pdata.entry[i].resource.category[0].coding[0].display,
-//                             address: ldata.address.text
-//                         }
-//                         localStorage.setItem(JSON.stringify(provider.name), [JSON.stringify(provider.name)])
-//                         if (pdata.entry[i].resource.specialty) {
-//                             let specialityArr = [];
-//                             for (let j = 0; j < pdata.entry[i].resource.specialty.length; j++) {
-//                                 specialityArr.push(pdata.entry[i].resource.specialty[j].text);
-//                             }
-//                             provider.speciality = specialityArr;
-//                         }
-//                         providerArr.push(provider)
-//                         // localStorage.setItem(JSON.stringify(providerArr[i].name));
-//                     })
-
-//             }
-            
-//             console.log("ProviderArr: ", providerArr[0]);
-//         })
-// }
 
 export default API;
