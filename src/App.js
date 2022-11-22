5fdcbff25b0fba9c6055f0e95a5eff10058d13b9
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2022-11-22 15:19:11 -0800

    some beginning table stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [advice, setAdvice] = useState("");
-  const [addressList, setAddressList] = useState([]);
-  const [providerList, setProviderList] = useState([]);
+  const [providerArray, setProviderArray] = useState("");
+  const [gotData, setGotData] = useState(false);
 
-  function showData(){
-    console.log(advice);
-    for(let i = 0; i<advice.length; i++) {
-      console.log(advice[i].resource.name)
+
+  function showData() {
+    console.log(providerArray);
+    setGotData(true);
+
+    for (let i = 0; i < providerArray.length; i++) {
+      console.log(providerArray[i].resource.name);
+      if(providerArray[i].resource.specialty !== undefined){
+      console.log(providerArray[i].resource.specialty[0].text)}
+
     }
   }
 
@@ -21,29 +26,70 @@ const App = () => {
         const response = await fetch(url);
         const json = await response.json();
         console.log(json.entry);
-        setAdvice(json.entry);
+        setProviderArray(json.entry);
 
-  
+
       } catch (error) {
         console.log("error", error);
       }
     };
 
     fetchData();
-    // console.log(provider);
+
+    // console.log(providerArray);
   }, []);
 
-  return (
-    <div>
-      {/* <p>{advice}</p> */}
-      {/* {addressList.map((address, i) => {
-        return <p key={i}>{address}</p>
-      })
 
-      } */}
-  <button onClick={showData}>Show Data</button>
-    </div>
-  );
+  if (gotData === true) {
+    return (
+      <div>
+        <p>{providerArray[0].resource.name}</p>
+        <table>
+          <tr>
+            <th>name</th>
+            <th>address</th>
+            <th>phone number</th>
+            <th>specialty</th>
+          </tr>
+          <tr>
+            <td>{providerArray[0].resource.name}</td>
+            <td><p>naah</p></td>
+            <td>{providerArray[0].resource.telecom[0].value}</td>
+            {/* <td>{providerArray[0].resource.specialty}</td> */}
+          </tr>
+        </table>
+
+      </div>
+
+
+    )
+    // providerArray.map((provider,index) => {
+    //   return (
+    // <div><h3 key={index}>{provider.resource.name}</h3> </div>) })
+  } else {
+    return (
+      <div>  <button onClick={showData}>Show Data</button></div>
+    )
+  }
+  //   return (
+  //     <div>
+  //       {/* <p>{providerArray}</p> */}
+  //       {/* {addressList.map((address, i) => {
+  //         return <p key={i}>{address}</p>
+  //       })
+
+  //       } */}
+  //   <button onClick={showData}>Show Data</button>
+
+  //   {gotData ? (
+  // providerArray.map((provider, i) => {
+  //   return (
+  // <div><p>{provider[i].resource.name}</p> </div>
+  //   )
+  // })
+  //   ): (<p>{gotData}</p>)}
+  //     </div>
+  //   );
 };
 
 export default App;
