953b6e36f5ca11efd7569f66dd3c1d2a19988554
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2022-11-28 20:08:33 -0800

    Updates

---
+++ precache-manifest.ad87e324567a579adb8e5ebc19bc0369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "54cd804437a38d62f3c6",
+    "url": "/api-test/static/css/main.a9927b77.chunk.css"
+  },
+  {
+    "revision": "54cd804437a38d62f3c6",
+    "url": "/api-test/static/js/main.54cd8044.chunk.js"
+  },
+  {
+    "revision": "69e0dab03ec9f6c99eb0",
+    "url": "/api-test/static/js/1.69e0dab0.chunk.js"
+  },
+  {
+    "revision": "100427401ea71565c2fa",
+    "url": "/api-test/static/js/2.10042740.chunk.js"
+  },
+  {
+    "revision": "a16128720f4f5e6fbf04",
+    "url": "/api-test/static/js/runtime~main.a1612872.js"
+  },
+  {
+    "revision": "a93d12d3ad165a84e74a148646b3232e",
+    "url": "/api-test/static/media/logo.a93d12d3.svg"
+  },
+  {
+    "revision": "fcba11a2c81f65fab201002f4bae6a87",
+    "url": "/api-test/index.html"
+  }
+];
