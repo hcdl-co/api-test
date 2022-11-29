f2a3fca49d0b13b7f5af2854b7ffa4b3d1bb36a9
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2022-11-28 16:08:55 -0800

    handle bad search added postalcode

--- src/App.js
+++ src/App.js
@@ -7,26 +7,53 @@ const App = () => {
   const [gotData, setGotData] = useState(false);
   const [locationURLs, getLocationURLs] = useState([])
   const [providerSpecialtyList, setProviderSpecialtyList] = useState([]);
-  const [currentSearch, setCurrentSearch] = useState('');
+
 
   const searchRef = useRef();
 
-  function handleChange(e) {
-    console.log(e.target.innerHTML)
-    // if (e.target.files) {
-    //     setState({ ...newInfo, [e.target.name]: e.target.files[0] });
-    // } else {
-    //     setState({ ...newInfo, [e.target.name]: e.target.value });
-    // }
+console.log(providerArray)
+
+const blank = providerArray;
+  function searchLocationPostalCode() {
+    console.log(providerArray);
+    setGotData(true);
+    console.log(searchRef.current.value)
+
+    let postalCode = searchRef.current.value
+    let url = `https://public.fhir.flex.optum.com/R4/HealthcareService?service-category=prov&location.address-postalcode=${postalCode}`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+
+        setProviderArray(json.entry);
+
+        let locURLArray = [];
+        json.entry.forEach(provider => locURLArray.push(provider.resource.location[0].reference))
+        getLocationURLs(locURLArray);
+
+        let specialtyArray = [];
+        json.entry.forEach(provider => {
+
+          if (provider.resource.specialty) {
+            specialtyArray.push(provider.resource.specialty[0].text)
+          } else {
+            specialtyArray.push('Not Available')
+          }
+          setProviderSpecialtyList(specialtyArray);
+        })
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
   }
 
   function showData() {
     console.log(providerArray);
     setGotData(true);
     console.log(searchRef.current.value)
-    setCurrentSearch(searchRef.current.value);
-
-
     let nameQuery = searchRef.current.value;
     let url = `https://public.fhir.flex.optum.com/R4/HealthcareService?service-category=prov&name=${nameQuery}`;
     const fetchData = async () => {
@@ -89,76 +116,85 @@ const App = () => {
 
 
 
-  if (gotData === true) {
+  // if (gotData === true) {
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>United Healthcare Providers</h1>
-          <p>brought to you by healthcare Download. Get in control, stay in control</p></header>
+          <p>brought to you by healthcare Download. Get in control, stay in control.</p></header>
         
           <input
             name="search"
             placeholder="search for another provider"
             ref={searchRef}
             id="searchInput"
-            onChange={handleChange}
           />
-          <button onClick={showData}>Show Data</button>
-
-        <table>
-          <tr>
-            <th>NAME</th>
-            <th>SPECIALTY</th>
-            <th>PHONE</th>
-            <th>ADDRESS</th>
-          </tr>
+          <button onClick={showData}>Search Name</button>
+          <button onClick={searchLocationPostalCode}> Search by Postal Code</button>
 
 
-
-          {providerArray.map((provider, index) => {
+          {providerArray !== undefined && providerArray.length ? (
+       
+            <table>
+          <thead>
+    <tr>
+              <th>NAME</th>
+              <th>SPECIALTY</th>
+              <th>PHONE</th>
+              <th>ADDRESS</th>
+              </tr>
+              </thead>
+            
+            {providerArray.map((provider, index) => {
 
             return (
               <tbody key={provider}>
-                <td key={index}>{provider.resource.name}</td>
-                <td key={index / 5}>{providerSpecialtyList[index]}</td>
+                  <tr>
+
+                <td key={index+ 1}>{provider.resource.name}</td>
+                <td key={index  + 10}>{providerSpecialtyList[index]}</td>
                 <td key={index * 10}>{provider.resource.telecom[0].value}</td>
                 <td><button key={index / 10} value={index} onClick={showAddress}>Show Address</button></td>
-
+                </tr>
 
               </tbody>
 
             )
-          })}
-
+          })
+        }
+        
         </table>
+          ) :(<div><p>Sorry, your search did not work, please try again or notify our team  at info@healthDL.com</p></div>)}
+
       </div>
 
 
     )
 
-  } else {
-    return (
+  // } else {
+  //   return (
 
-      <div className="App">
-        <header className="App-header">
-          <h1>Search for a United Healthcare Provider</h1>
-          <p>brought to you by Healthcare Download. Get in control, stay in control.</p>
-        </header>
-        <main>
-          <input
-            name="search"
-            placeholder="search for a provider"
-            ref={searchRef}
-            id="searchInput"
-            onChange={handleChange}
-          />
-          <button onClick={showData}>Show Data</button>
-        </main>
-      </div>
-    )
-  }
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <h1>Search for a United Healthcare Provider</h1>
+  //         <p>brought to you by Healthcare Download. Get in control, stay in control.</p>
+  //       </header>
+  //       <main>
+  //         <input
+  //           name="search"
+  //           placeholder="provider last name"
+  //           ref={searchRef}
+  //           id="searchInput"
+ 
+  //         />
+  //         <button onClick={showData}>Show Data</button>
+  //         <button onClick={searchLocationPostalCode}> Search By Postal Code</button>
+  //       </main>
+  //     </div>
+  //   )
+  // }
 };
 
 export default App;
