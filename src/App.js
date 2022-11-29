21ba1d279ea76fd83e618e853103347ff4a2e62a
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2022-11-28 16:56:18 -0800

    styled search inputs

--- src/App.js
+++ src/App.js
@@ -4,19 +4,129 @@ import './App.css';
 
 const App = () => {
   const [providerArray, setProviderArray] = useState([]);
-  const [gotData, setGotData] = useState(false);
+
   const [locationURLs, getLocationURLs] = useState([])
   const [providerSpecialtyList, setProviderSpecialtyList] = useState([]);
 
 
   const searchRef = useRef();
 
-console.log(providerArray)
+  const nameRef = useRef();
+  const cityRef = useRef();
+  const stateRef = useRef();
+  const postalRef = useRef();
 
-const blank = providerArray;
+
+  console.log(providerArray)
+
+
   function searchLocationPostalCode() {
     console.log(providerArray);
-    setGotData(true);
+
+    let postalCode = searchRef.current.value
+    let url = `https://public.fhir.flex.optum.com/R4/HealthcareService?service-category=prov&location.address-postalcode=${postalCode}`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+
+        setProviderArray(json.entry);
+
+        let locURLArray = [];
+        json.entry.forEach(provider => locURLArray.push(provider.resource.location[0].reference))
+        getLocationURLs(locURLArray);
+
+        let specialtyArray = [];
+        json.entry.forEach(provider => {
+
+          if (provider.resource.specialty) {
+            specialtyArray.push(provider.resource.specialty[0].text)
+          } else {
+            specialtyArray.push('Not Available')
+          }
+          setProviderSpecialtyList(specialtyArray);
+        })
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }
+  function searchSpecialty() {
+    console.log(providerArray);
+
+    console.log(searchRef.current.value)
+
+    let postalCode = searchRef.current.value
+    let url = `https://public.fhir.flex.optum.com/R4/HealthcareService?service-category=prov&specialty=${postalCode}`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        // setProviderArray(json.entry);
+
+        // let locURLArray = [];
+        // json.entry.forEach(provider => locURLArray.push(provider.resource.location[0].reference))
+        // getLocationURLs(locURLArray);
+
+        // let specialtyArray = [];
+        // json.entry.forEach(provider => {
+
+        //   if (provider.resource.specialty) {
+        //     specialtyArray.push(provider.resource.specialty[0].text)
+        //   } else {
+        //     specialtyArray.push('Not Available')
+        //   }
+        //   setProviderSpecialtyList(specialtyArray);
+        // })
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }
+  function searchCity() {
+    console.log(providerArray);
+
+    console.log(searchRef.current.value)
+
+    let postalCode = searchRef.current.value
+    let url = `https://public.fhir.flex.optum.com/R4/HealthcareService?service-category=prov&location.address-postalcode=${postalCode}`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+
+        setProviderArray(json.entry);
+
+        let locURLArray = [];
+        json.entry.forEach(provider => locURLArray.push(provider.resource.location[0].reference))
+        getLocationURLs(locURLArray);
+
+        let specialtyArray = [];
+        json.entry.forEach(provider => {
+
+          if (provider.resource.specialty) {
+            specialtyArray.push(provider.resource.specialty[0].text)
+          } else {
+            specialtyArray.push('Not Available')
+          }
+          setProviderSpecialtyList(specialtyArray);
+        })
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }
+
+  function searchLocationPostalCode() {
+    console.log(providerArray);
+
     console.log(searchRef.current.value)
 
     let postalCode = searchRef.current.value
@@ -52,7 +162,7 @@ const blank = providerArray;
 
   function showData() {
     console.log(providerArray);
-    setGotData(true);
+
     console.log(searchRef.current.value)
     let nameQuery = searchRef.current.value;
     let url = `https://public.fhir.flex.optum.com/R4/HealthcareService?service-category=prov&name=${nameQuery}`;
@@ -118,60 +228,76 @@ const blank = providerArray;
 
   // if (gotData === true) {
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>United Healthcare Providers</h1>
-          <p>brought to you by healthcare Download. Get in control, stay in control.</p></header>
-        
-          <input
-            name="search"
-            placeholder="search for another provider"
-            ref={searchRef}
-            id="searchInput"
-          />
-          <button onClick={showData}>Search Name</button>
-          <button onClick={searchLocationPostalCode}> Search by Postal Code</button>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>United Healthcare Providers</h1>
+        <p>brought to you by healthcare Download. Get in control, stay in control.</p></header>
 
+      <input
+        name="name"
+        placeholder="search by name"
+        ref={searchRef}
+        id="searchInput"
+      />
+        <input
+        name="city"
+        placeholder="search by city"
+        ref={searchRef}
+        id="searchInput"
+      />
+        <input
+        name="state"
+        placeholder="search by state"
+        ref={searchRef}
+        id="searchInput"
+      />
+        <input
+        name="search"
+        placeholder="search by postal code"
+        ref={searchRef}
+        id="searchInput"
+      />
 
-          {providerArray !== undefined && providerArray.length ? (
-       
-            <table>
+      <button onClick={showData}>Find A Doctor</button>
+      {providerArray !== undefined && providerArray.length ? (
+
+        <table>
           <thead>
-    <tr>
+            <tr>
               <th>NAME</th>
               <th>SPECIALTY</th>
               <th>PHONE</th>
               <th>ADDRESS</th>
-              </tr>
-              </thead>
-            
-            {providerArray.map((provider, index) => {
+            </tr>
+          </thead>
+
+          {providerArray.map((provider, index) => {
 
             return (
               <tbody key={provider.fullUrl}>
-                  <tr>
+                <tr>
 
-                <td key={provider.resource.name}>{provider.resource.name}</td>
-                <td key={providerSpecialtyList[index]}>{providerSpecialtyList[index]}</td>
-                <td key={provider.resource.telecom[0].value}>{provider.resource.telecom[0].value}</td>
-                <td><button key={index} value={index} onClick={showAddress}>Show Address</button></td>
+                  <td key={provider.resource.name}>{provider.resource.name}</td>
+                  <td key={providerSpecialtyList[index]}>{providerSpecialtyList[index]}</td>
+                  <td key={provider.resource.telecom[0].value}>{provider.resource.telecom[0].value}</td>
+                  <td><button key={index} value={index} onClick={showAddress}>Show Address</button></td>
                 </tr>
 
               </tbody>
 
             )
           })
-        }
-        
+          }
+
         </table>
-          ) :(<div><p>Sorry, your search did not work, please try again or notify our team  at info@healthDL.com</p></div>)}
+      ) : (<div><p>Sorry, your search did not work, please try again or notify our team  at info@healthcareDL.com</p></div>)}
 
-      </div>
+    </div>
 
 
-    )
+  )
 
   // } else {
   //   return (
@@ -187,7 +313,7 @@ const blank = providerArray;
   //           placeholder="provider last name"
   //           ref={searchRef}
   //           id="searchInput"
- 
+
   //         />
   //         <button onClick={showData}>Show Data</button>
   //         <button onClick={searchLocationPostalCode}> Search By Postal Code</button>
